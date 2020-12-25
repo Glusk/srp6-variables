36d2d4eaa3fb883d9efbc7ba4adfe190fef3d133
Glusk <lj.slo@hotmail.com>
2020-12-25 04:41:01 +0100

    #6 Added test for little-endian zero-pad

--- src/test/java/com.github.glusk.srp6_variables/com/github/glusk/srp6_variables/SRP6VariableTest.java
+++ src/test/java/com.github.glusk.srp6_variables/com/github/glusk/srp6_variables/SRP6VariableTest.java
@@ -11,15 +11,29 @@ import org.junit.jupiter.api.Test;
 
 public final class SRP6VariableTest {
     @Test
-    public void zeroPadsToSize() {
+    public void zeroPadsToBigEndianToSize() {
         assertArrayEquals(
             new byte[] {0, 0, 1, 2, 0},
             new SRP6Variable() {
                 @Override
                 public Bytes bytes(final ByteOrder order) {
+                    // Big-endian representation
                     return () -> new byte[] {1, 2, 0};
                 }
             }.bytes(ByteOrder.BIG_ENDIAN, 5).asArray()
+        );
+    }
+    @Test
+    public void zeroPadsLittleEndianToSize() {
+        assertArrayEquals(
+            new byte[] {0, 2, 1, 0, 0},
+            new SRP6Variable() {
+                @Override
+                public Bytes bytes(final ByteOrder order) {
+                    // Little-endian representation
+                    return () -> new byte[] {0, 2, 1};
+                }
+            }.bytes(ByteOrder.LITTLE_ENDIAN, 5).asArray()
         );
     }
     @Test
@@ -30,6 +44,7 @@ public final class SRP6VariableTest {
                 new SRP6Variable() {
                     @Override
                     public Bytes bytes(final ByteOrder order) {
+                        // Big-endian representation
                         return () -> new byte[] {1, 2, 0};
                     }
                 }.bytes(ByteOrder.BIG_ENDIAN, 2).asArray();
