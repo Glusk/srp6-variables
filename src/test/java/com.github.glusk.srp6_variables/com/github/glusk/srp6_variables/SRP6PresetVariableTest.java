bc7d81ffc3c3f3f053428a8aa6c2d8ad164d3e06
Glusk <8526099+Glusk@users.noreply.github.com>
2020-12-25 05:05:20 +0100

    Improve coverage (#7)
    * #6 Always make a copy
    
    * #6 Test a BigInteger argument
    
    * #6 Test for little-endian result
    
    * #6 Added test for little-endian zero-pad
    
    * #6 Added BigInteger test
    
    * #6 typo
    
    * #6 another typo

--- src/test/java/com.github.glusk.srp6_variables/com/github/glusk/srp6_variables/SRP6PresetVariableTest.java
+++ src/test/java/com.github.glusk.srp6_variables/com/github/glusk/srp6_variables/SRP6PresetVariableTest.java
@@ -2,6 +2,7 @@ package com.github.glusk.srp6_variables;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
+import java.math.BigInteger;
 import java.nio.ByteOrder;
 
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,25 @@ public final class SRP6PresetVariableTest {
                 () -> new byte[] {0, 1, 2, 0},
                 ByteOrder.BIG_ENDIAN
             ).bytes(ByteOrder.BIG_ENDIAN).asArray()
+        );
+    }
+    @Test
+    public void returnsLittleEndianByteSequence() {
+        assertArrayEquals(
+            new byte[] {0, 2, 1},
+            new SRP6PresetVariable(
+                () -> new byte[] {1, 2, 0},
+                ByteOrder.BIG_ENDIAN
+            ).bytes(ByteOrder.LITTLE_ENDIAN).asArray()
+        );
+    }
+    @Test
+    public void zeroReturnsAnEmptyByteSequence() {
+        assertArrayEquals(
+            new byte[0],
+            new SRP6PresetVariable(BigInteger.ZERO)
+                .bytes(ByteOrder.BIG_ENDIAN)
+                .asArray()
         );
     }
 }
--- src/test/java/com.github.glusk.srp6_variables/com/github/glusk/srp6_variables/SRP6VariableTest.java
+++ src/test/java/com.github.glusk.srp6_variables/com/github/glusk/srp6_variables/SRP6VariableTest.java
@@ -1,8 +1,10 @@
 package com.github.glusk.srp6_variables;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.math.BigInteger;
 import java.nio.ByteOrder;
 
 import com.github.glusk.caesar.Bytes;
@@ -11,15 +13,29 @@ import org.junit.jupiter.api.Test;
 
 public final class SRP6VariableTest {
     @Test
-    public void zeroPadsToSize() {
+    public void zeroPadsBigEndianToSize() {
         assertArrayEquals(
             new byte[] {0, 0, 1, 2, 0},
             new SRP6Variable() {
                 @Override
                 public Bytes bytes(final ByteOrder order) {
+                    // Big-endian representation
                     return () -> new byte[] {1, 2, 0};
                 }
             }.bytes(ByteOrder.BIG_ENDIAN, 5).asArray()
+        );
+    }
+    @Test
+    public void zeroPadsLittleEndianToSize() {
+        assertArrayEquals(
+            new byte[] {0, 2, 1, 0, 0},
+            new SRP6Variable() {
+                @Override
+                public Bytes bytes(final ByteOrder order) {
+                    // Little-endian representation
+                    return () -> new byte[] {0, 2, 1};
+                }
+            }.bytes(ByteOrder.LITTLE_ENDIAN, 5).asArray()
         );
     }
     @Test
@@ -30,10 +46,23 @@ public final class SRP6VariableTest {
                 new SRP6Variable() {
                     @Override
                     public Bytes bytes(final ByteOrder order) {
+                        // Big-endian representation
                         return () -> new byte[] {1, 2, 0};
                     }
                 }.bytes(ByteOrder.BIG_ENDIAN, 2).asArray();
             }
+        );
+    }
+    @Test
+    public void returnsAsBigInteger() {
+        assertEquals(
+            BigInteger.ZERO,
+            new SRP6Variable() {
+                @Override
+                public Bytes bytes(final ByteOrder order) {
+                    return () -> new byte[0];
+                }
+            }.asNonNegativeBigInteger()
         );
     }
 }
