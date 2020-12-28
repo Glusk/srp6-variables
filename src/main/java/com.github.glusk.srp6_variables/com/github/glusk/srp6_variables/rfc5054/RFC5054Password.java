b2326ceb5d403381e72e657d7832864812d8e162
Glusk <lj.slo@hotmail.com>
2020-12-28 20:58:10 +0100

    #12 Added RFC 5054 Test Vectors

---
+++ src/main/java/com.github.glusk.srp6_variables/com/github/glusk/srp6_variables/rfc5054/RFC5054Password.java
@@ -0,0 +1,16 @@
+package com.github.glusk.srp6_variables.rfc5054;
+
+import com.github.glusk.caesar.Bytes;
+import com.github.glusk.caesar.PlainText;
+
+/** RFC5054 Test Vector: cleartext password (P). */
+public final class RFC5054Password implements Bytes {
+    /** Pre-set constant that represents this variable. */
+    private static final Bytes VALUE =
+        new PlainText("password123");
+
+    @Override
+    public byte[] asArray() {
+        return VALUE.asArray();
+    }
+}
