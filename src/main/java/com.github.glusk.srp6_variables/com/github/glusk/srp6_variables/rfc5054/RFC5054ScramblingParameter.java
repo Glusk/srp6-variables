d74096b7e1518943a340834105008701150312f8
Glusk <lj.slo@hotmail.com>
2020-12-30 10:43:40 +0100

    #16 Add RFC 5054 Test Vectors

---
+++ src/main/java/com.github.glusk.srp6_variables/com/github/glusk/srp6_variables/rfc5054/RFC5054ScramblingParameter.java
@@ -0,0 +1,25 @@
+package com.github.glusk.srp6_variables.rfc5054;
+
+import java.nio.ByteOrder;
+
+import com.github.glusk.caesar.Bytes;
+import com.github.glusk.caesar.Hex;
+
+import com.github.glusk.srp6_variables.SRP6IntegerVariable;
+import com.github.glusk.srp6_variables.SRP6PresetIntegerVariable;
+
+/** RFC5054 Test Vector: random scrambling parameter (u). */
+public final class RFC5054ScramblingParameter implements SRP6IntegerVariable {
+    /** Pre-set constant that represents this variable. */
+    private static final SRP6IntegerVariable VALUE =
+        new SRP6PresetIntegerVariable(
+            new Hex("CE38B959 3487DA98 554ED47D 70A7AE5F 462EF019"),
+            ByteOrder.BIG_ENDIAN
+        );
+
+    @Override
+    public Bytes bytes(final ByteOrder preferredOrder) {
+        return VALUE.bytes(preferredOrder);
+    }
+}
+
