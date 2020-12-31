a1229b0fe3c49ef265d0e1edf79eb9cf198fd79d
Glusk <lj.slo@hotmail.com>
2020-12-31 14:23:41 +0100

    #18 Add Random Scrambling Parameter (u) test

--- src/test/java/com.github.glusk.srp6_variables/com/github/glusk/srp6_variables/SRP6ScramblingParameterTest.java
+++ src/test/java/com.github.glusk.srp6_variables/com/github/glusk/srp6_variables/SRP6ScramblingParameterTest.java
@@ -8,12 +8,17 @@ import java.security.NoSuchAlgorithmException;
 
 import org.junit.jupiter.api.Test;
 
+import com.github.glusk.caesar.PlainText;
+import com.github.glusk.caesar.hashing.Hash;
 import com.github.glusk.caesar.hashing.ImmutableMessageDigest;
 
 import com.github.glusk.srp6_variables.rfc5054.RFC5054ClientPublicKey;
 import com.github.glusk.srp6_variables.rfc5054.RFC5054ScramblingParameter;
 import com.github.glusk.srp6_variables.rfc5054.RFC5054ServerPublicKey;
 
+import com.github.glusk.srp6_variables.wiki.WikiClientPublicKey;
+import com.github.glusk.srp6_variables.wiki.WikiScramblingParameter;
+import com.github.glusk.srp6_variables.wiki.WikiServerPublicKey;
 
 public final class SRP6ScramblingParameterTest {
     @Test
@@ -31,6 +36,34 @@ public final class SRP6ScramblingParameterTest {
                 )
             ),
             "Computed variable does not match the RFC5054 Test Vector"
+        );
+    }
+    @Test
+    public void testAgainstWikipediaExampleVariables()
+        throws NoSuchAlgorithmException {
+        assertTrue(
+            new WikiScramblingParameter().equals(
+                new SRP6PresetIntegerVariable(
+                    new Hash(
+                        new ImmutableMessageDigest(
+                            MessageDigest.getInstance("SHA-256")
+                        ),
+                        new PlainText(
+                            new WikiClientPublicKey()
+                                .asNonNegativeBigInteger()
+                                .toString()
+                        ),
+                        new PlainText(":"),
+                        new PlainText(
+                            new WikiServerPublicKey()
+                                .asNonNegativeBigInteger()
+                                .toString()
+                        )
+                    ),
+                    ByteOrder.BIG_ENDIAN
+                )
+            ),
+            "Computed variable does not match the Wikipedia example variable"
         );
     }
 }
