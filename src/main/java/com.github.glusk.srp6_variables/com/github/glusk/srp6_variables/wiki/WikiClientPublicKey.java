29bf524098b03c2cd3ead3b9ecafadfb54b4c850
Glusk <lj.slo@hotmail.com>
2020-12-31 14:23:17 +0100

    #18 Add more test variables

---
+++ src/main/java/com.github.glusk.srp6_variables/com/github/glusk/srp6_variables/wiki/WikiClientPublicKey.java
@@ -0,0 +1,34 @@
+package com.github.glusk.srp6_variables.wiki;
+
+import java.nio.ByteOrder;
+
+import com.github.glusk.caesar.Bytes;
+import com.github.glusk.caesar.Hex;
+
+import com.github.glusk.srp6_variables.SRP6IntegerVariable;
+import com.github.glusk.srp6_variables.SRP6PresetIntegerVariable;
+
+/** Wikipedia Python example variable: client ephemeral public key (A). */
+public final class WikiClientPublicKey implements SRP6IntegerVariable {
+    /** Pre-set constant that represents this variable. */
+    private static final SRP6IntegerVariable VALUE =
+        new SRP6PresetIntegerVariable(
+            new Hex(
+                "48:14:7d:01:3e:3a:2e:08:ac:e2:22:a0:ab:91:4a:"
+              + "7e:d6:7c:70:4b:24:80:71:6b:53:f9:d2:29:24:3d:"
+              + "17:25:47:3c:f4:45:19:04:65:85:97:f4:87:b0:fa:"
+              + "8b:c7:d5:44:67:1b:25:56:3f:09:5b:ad:38:4c:bb:"
+              + "8d:a7:f5:8f:7f:13:c8:fa:8b:b9:d6:aa:de:5f:e0:"
+              + "2d:f2:88:f2:b3:8d:71:d5:10:36:ed:e5:28:02:64:"
+              + "5f:82:cd:72:16:53:5c:0c:97:8f:90:23:0e:0f:87:"
+              + "81:63:a6:38:cf:57:ad:11:96:81:69:c2:6e:46:7b:"
+              + "8e:e1:4e:b2:ca:5b:16:14"
+            ),
+            ByteOrder.BIG_ENDIAN
+        );
+
+    @Override
+    public Bytes bytes(final ByteOrder preferredOrder) {
+        return VALUE.bytes(preferredOrder);
+    }
+}
---
+++ src/main/java/com.github.glusk.srp6_variables/com/github/glusk/srp6_variables/wiki/WikiServerPublicKey.java
@@ -0,0 +1,34 @@
+package com.github.glusk.srp6_variables.wiki;
+
+import java.nio.ByteOrder;
+
+import com.github.glusk.caesar.Bytes;
+import com.github.glusk.caesar.Hex;
+
+import com.github.glusk.srp6_variables.SRP6IntegerVariable;
+import com.github.glusk.srp6_variables.SRP6PresetIntegerVariable;
+
+/** Wikipedia Python example variable: server ephemeral public key (B). */
+public final class WikiServerPublicKey implements SRP6IntegerVariable {
+    /** Pre-set constant that represents this variable. */
+    private static final SRP6IntegerVariable VALUE =
+        new SRP6PresetIntegerVariable(
+            new Hex(
+                "70:9f:34:07:38:e6:2e:46:18:46:34:ac:d2:cd:7c:"
+              + "86:1a:7d:92:c5:fd:e9:eb:43:ac:12:02:26:a0:eb:"
+              + "66:01:ee:5d:1a:0b:92:ff:b6:25:41:70:d9:1f:b4:"
+              + "51:c3:c0:2b:bf:8b:41:f9:e7:e3:e8:85:d7:09:f0:"
+              + "dc:48:08:04:8e:59:5c:68:44:8a:21:11:b4:5e:ef:"
+              + "aa:1e:2d:6a:48:14:d9:9a:e0:38:a5:f2:37:1c:75:"
+              + "3b:31:2c:52:9c:ad:a6:6b:23:e6:51:2c:7e:f8:14:"
+              + "68:3f:4c:fe:2a:4c:54:13:c4:34:e2:1b:c6:89:d8:"
+              + "69:fc:96:91:41:b8:4a:61"
+            ),
+            ByteOrder.BIG_ENDIAN
+        );
+
+    @Override
+    public Bytes bytes(final ByteOrder preferredOrder) {
+        return VALUE.bytes(preferredOrder);
+    }
+}
