07a6713ccaf6a61d8c6751a80d153a0fc3d0a4b8
Glusk <lj.slo@hotmail.com>
2021-01-03 00:51:21 +0100

    #30 Added multiplier

---
+++ src/main/java/com.github.glusk.srp6_variables/com/github/glusk/srp6_variables/SRP6Multiplier.java
@@ -0,0 +1,85 @@
+package com.github.glusk.srp6_variables;
+
+import java.nio.ByteOrder;
+
+import com.github.glusk.caesar.Bytes;
+import com.github.glusk.caesar.hashing.Hash;
+import com.github.glusk.caesar.hashing.ImmutableMessageDigest;
+
+/**
+ * SRP-6 Multiplier Parameter (k).
+ * <p>
+ * This variable is computed as:
+ * <pre>
+ * k = 3
+ * </pre>
+ * in SRP-6, and as:
+ * <pre>
+ * k = H(N | g)
+ * </pre>
+ * in SRP-6a.
+ * <p>
+ * Refer to the  {@link com.github.glusk.srp6_variables package docs} for more
+ * info on notation used.
+ */
+public final class SRP6Multiplier implements SRP6IntegerVariable {
+    /** SRP-6 Integer Variable: multiplier parameter (k). */
+    private final SRP6IntegerVariable multiplier;
+
+    /**
+     * Constructs a new SRP-6 Multiplier Parameter as specified in RFC 5054.
+     * <pre>
+     * u = H(N | PAD(g))
+     * </pre>
+     * <strong>Note:</strong> {@code g} is zero-padded to the byte length of
+     * {@code N}.
+     *
+     * @param hashFunction a one-way hash function - H()
+     * @param prime SRP-6 variable: prime (N)
+     * @param generator SRP-6 variable: generator (g)
+     * @param endianness the byte order to use when converting the resulting
+     *                   hash to integer and the byte order of prime and
+     *                   generator byte sequences to feed to the hash function
+     */
+    public SRP6Multiplier(
+        final ImmutableMessageDigest hashFunction,
+        final SRP6IntegerVariable prime,
+        final SRP6IntegerVariable generator,
+        final ByteOrder endianness
+    ) {
+        this(
+            new SRP6CustomIntegerVariable(
+                new Hash(
+                    hashFunction,
+                    prime.bytes(endianness),
+                    generator.bytes(
+                        endianness,
+                        prime.bytes(endianness).asArray().length
+                    )
+                ),
+                endianness
+            )
+        );
+    }
+
+    /**
+     * Hidden constructor.
+     * <p>
+     * Users should not be able to set this variable directly. Constants and
+     * preset values must be created by using the
+     * {@link SRP6PresetIntegerVariable} class.
+     *
+     * @param multiplier SRP-6 Integer Variable: multiplier parameter (k)
+     */
+    @SuppressWarnings("checkstyle:hiddenfield")
+    private SRP6Multiplier(
+        final SRP6IntegerVariable multiplier
+    ) {
+        this.multiplier = multiplier;
+    }
+
+    @Override
+    public Bytes bytes(final ByteOrder preferredOrder) {
+        return multiplier.bytes(preferredOrder);
+    }
+}
