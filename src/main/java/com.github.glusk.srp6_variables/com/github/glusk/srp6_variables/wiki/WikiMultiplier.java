4ac4bd1a77b510838a33b0d5101db17c3ca36214
Glusk <8526099+Glusk@users.noreply.github.com>
2021-01-03 00:58:47 +0100

    Multiplier (#31)
    * #30 Added multiplier
    
    * #20 missing doc updates
    
    * #30 typo

---
+++ src/main/java/com.github.glusk.srp6_variables/com/github/glusk/srp6_variables/wiki/WikiMultiplier.java
@@ -0,0 +1,27 @@
+package com.github.glusk.srp6_variables.wiki;
+
+import java.nio.ByteOrder;
+
+import com.github.glusk.caesar.Bytes;
+import com.github.glusk.caesar.Hex;
+
+import com.github.glusk.srp6_variables.SRP6IntegerVariable;
+import com.github.glusk.srp6_variables.SRP6CustomIntegerVariable;
+
+/** Wikipedia Python example variable: multiplier parameter (k).*/
+public final class WikiMultiplier implements SRP6IntegerVariable {
+    /** Pre-set constant that represents this variable. */
+    private static final SRP6IntegerVariable VALUE =
+        new SRP6CustomIntegerVariable(
+            new Hex(
+                "b317ec55 3cb1a522 01d79b7c 12d4b665"
+              + "d0dc234f dbfd5a06 894c1a19 4f818c4a"
+            ),
+            ByteOrder.BIG_ENDIAN
+        );
+
+    @Override
+    public Bytes bytes(final ByteOrder preferredOrder) {
+        return VALUE.bytes(preferredOrder);
+    }
+}
---
+++ src/test/java/com.github.glusk.srp6_variables/com/github/glusk/srp6_variables/SRP6MultiplierTest.java
@@ -0,0 +1,69 @@
+package com.github.glusk.srp6_variables;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.nio.ByteOrder;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.junit.jupiter.api.Test;
+
+import com.github.glusk.caesar.PlainText;
+import com.github.glusk.caesar.hashing.Hash;
+import com.github.glusk.caesar.hashing.ImmutableMessageDigest;
+
+import com.github.glusk.srp6_variables.rfc5054.RFC5054Generator;
+import com.github.glusk.srp6_variables.rfc5054.RFC5054Multiplier;
+import com.github.glusk.srp6_variables.rfc5054.RFC5054Prime;
+
+import com.github.glusk.srp6_variables.wiki.WikiGenerator;
+import com.github.glusk.srp6_variables.wiki.WikiMultiplier;
+import com.github.glusk.srp6_variables.wiki.WikiPrime;
+
+public final class SRP6MultiplierTest {
+    @Test
+    public void testAgainstRFC5054TestVectors()
+        throws NoSuchAlgorithmException {
+        assertTrue(
+            new RFC5054Multiplier().equals(
+                new SRP6Multiplier(
+                    new ImmutableMessageDigest(
+                        MessageDigest.getInstance("SHA-1")
+                    ),
+                    new RFC5054Prime(),
+                    new RFC5054Generator(),
+                    ByteOrder.BIG_ENDIAN
+                )
+            ),
+            "Computed variable does not match the RFC5054 Test Vector"
+        );
+    }
+    @Test
+    public void testAgainstWikipediaExampleVariables()
+        throws NoSuchAlgorithmException {
+        assertTrue(
+            new WikiMultiplier().equals(
+                new SRP6CustomIntegerVariable(
+                    new Hash(
+                        new ImmutableMessageDigest(
+                            MessageDigest.getInstance("SHA-256")
+                        ),
+                        new PlainText(
+                            new WikiPrime()
+                                .asNonNegativeBigInteger()
+                                .toString()
+                        ),
+                        new PlainText(":"),
+                        new PlainText(
+                            new WikiGenerator()
+                                .asNonNegativeBigInteger()
+                                .toString()
+                        )
+                    ),
+                    ByteOrder.BIG_ENDIAN
+                )
+            ),
+            "Computed variable does not match the Wikipedia example variable"
+        );
+    }
+}
