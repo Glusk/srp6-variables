2915f849f0db9fb415823754a5bfc0a9bd205f76
Glusk <lj.slo@hotmail.com>
2021-01-09 12:54:10 +0100

    #26 Added Random Ephemeral (a, b)

---
+++ src/test/java/com.github.glusk.srp6_variables/com/github/glusk/srp6_variables/SRP6RandomEphemeralTest.java
@@ -0,0 +1,54 @@
+package com.github.glusk.srp6_variables;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.security.SecureRandom;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
+
+import com.github.glusk.srp6_variables.rfc5054.RFC5054Prime;
+
+public final class SRP6RandomEphemeralTest {
+    @Test
+    public void returnsTheSameValueOnMultipleInvocations() {
+        SRP6IntegerVariable r =
+            new SRP6RandomEphemeral(
+                new SecureRandom(),
+                -1,
+                new RFC5054Prime()
+            );
+        assertEquals(
+            r.asNonNegativeBigInteger(),
+            r.asNonNegativeBigInteger(),
+            "Subsequent invocations don't return the same value."
+        );
+    }
+    @Test
+    public void throwsIfBitLengthIsTooLong() {
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> {
+                new SRP6RandomEphemeral(
+                    new SecureRandom(),
+                    1024 + 1,
+                    new RFC5054Prime()
+                );
+            }
+        );
+    }
+    @RepeatedTest(100)
+    public void defaultsToMinimumBitLength() {
+        assertEquals(
+            256,
+            new SRP6RandomEphemeral(
+                new SecureRandom(),
+                -1,
+                new RFC5054Prime()
+            )
+            .asNonNegativeBigInteger()
+            .bitLength()
+        );
+    }
+}
