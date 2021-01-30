f6ec465b131be60c0610b0a78504e44aa349cb7b
Glusk <lj.slo@hotmail.com>
2021-01-30 16:48:26 +0100

    #68 Extend Client Session Proof functionality

---
+++ src/main/java/com.github.glusk.srp6_variables/com/github/glusk/srp6_variables/mozilla/MozillaClientSessionProof.java
@@ -0,0 +1,19 @@
+package com.github.glusk.srp6_variables.mozilla;
+
+import com.github.glusk.caesar.Bytes;
+import com.github.glusk.caesar.Hex;
+
+/** Mozilla Test Vector: client session proof (M1). */
+public final class MozillaClientSessionProof implements Bytes {
+    /** Pre-set constant that represents this variable. */
+    private static final Bytes VALUE =
+        new Hex(
+            "27949ec1e0f16256 33436865edb037e2"
+          + "3eb6bf5cb91873f2 a2729373c2039008"
+        );
+
+    @Override
+    public byte[] asArray() {
+        return VALUE.asArray();
+    }
+}
