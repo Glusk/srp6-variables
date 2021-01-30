8ea2c92e43fa37762cf388c83797cc6c173978ae
Glusk <lj.slo@hotmail.com>
2021-01-30 17:19:37 +0100

    #68 Add Session Key Test

---
+++ src/main/java/com.github.glusk.srp6_variables/com/github/glusk/srp6_variables/mozilla/MozillaSessionKey.java
@@ -0,0 +1,19 @@
+package com.github.glusk.srp6_variables.mozilla;
+
+import com.github.glusk.caesar.Bytes;
+import com.github.glusk.caesar.Hex;
+
+/** Mozilla Test Vector: srpK (K). */
+public final class MozillaSessionKey implements Bytes {
+    /** Pre-set constant that represents this variable. */
+    private static final Bytes VALUE =
+        new Hex(
+            "e68fd0112bfa31dc ffc8e9c96a1cbadb"
+          + "4c3145978ff35c73 e5bf8d30bbc7499a"
+        );
+
+    @Override
+    public byte[] asArray() {
+        return VALUE.asArray();
+    }
+}
--- src/test/java/com.github.glusk.srp6_variables/com/github/glusk/srp6_variables/SRP6SessionKeyTest.java
+++ src/test/java/com.github.glusk.srp6_variables/com/github/glusk/srp6_variables/SRP6SessionKeyTest.java
@@ -13,6 +13,10 @@ import com.github.glusk.caesar.PlainText;
 import com.github.glusk.caesar.hashing.Hash;
 import com.github.glusk.caesar.hashing.ImmutableMessageDigest;
 
+import com.github.glusk.srp6_variables.mozilla.MozillaSessionKey;
+import com.github.glusk.srp6_variables.mozilla.MozillaSharedSecret;
+import com.github.glusk.srp6_variables.mozilla.MozillaPrime;
+
 import com.github.glusk.srp6_variables.wiki.WikiSessionKey;
 import com.github.glusk.srp6_variables.wiki.WikiPremasterSecret;
 
@@ -20,6 +24,25 @@ import com.github.glusk.srp6_variables.wow.WoWPremasterSecret;
 import com.github.glusk.srp6_variables.wow.WoWSessionKey;
 
 public final class SRP6SessionKeyTest {
+    @Test
+    public void testAgainstMozillaExampleVariables()
+        throws NoSuchAlgorithmException, SRP6PaddingException {
+        assertArrayEquals(
+            new MozillaSessionKey().asArray(),
+            new Hash(
+                new ImmutableMessageDigest(
+                    MessageDigest.getInstance("SHA-256")
+                ),
+                new MozillaSharedSecret().bytes(
+                    ByteOrder.BIG_ENDIAN,
+                    new MozillaPrime().bytes(
+                        ByteOrder.BIG_ENDIAN
+                    ).asArray().length
+                )
+            ).asArray(),
+            "Computed variable does not match the Mozilla example variable"
+        );
+    }
     @Test
     public void testAgainstWikipediaExampleVariables()
         throws NoSuchAlgorithmException {
